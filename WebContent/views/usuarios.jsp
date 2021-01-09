599b7236a684605e1e3b5fda8ccc457b1d52d6c0
hermancortez <38408445+hermancortez@users.noreply.github.com>
2021-01-08 21:10:55 -0300

    Commit 4
    Se agrega
    - UsuarioController.java
    - usuarios.jsp

---
+++ WebContent/views/usuarios.jsp
@@ -0,0 +1,38 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Listado de Usuarios</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+	integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<h1>Listado de Usuarios</h1>
+		<table class="table">
+			<thead class="table-dark">
+				<tr>
+					<th scope="col">Id</th>
+					<th scope="col">Nombres</th>
+					<th scope="col">Apellidos</th>
+					<th scope="col">Fecha Nacimiento</th>
+					<th scope="col">Tipo de Usuario</th>
+			</thead>
+			<tbody>
+				<c:forEach var="c" items="${usuario}">
+					<tr>
+						<td><c:out value="${c.getId_reg()}"></c:out></td>
+						<td><c:out value="${c.getNombres()}"></c:out></td>
+						<td><c:out value="${c.getApellidos()}"></c:out></td>
+						<td><c:out value="${c.getFecha_nac()}"></c:out></td>
+						<td><c:out value="${c.getTipo_usuario()}"></c:out></td>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
+	</div>
+</body>
+</html>
---
+++ src/controller/UsuarioController.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Usuario;
+import model.UsuarioDAO;
+
+/**
+ * Servlet implementation class UsuarioController
+ */
+@WebServlet("/UsuarioController")
+public class UsuarioController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UsuarioController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		UsuarioDAO usuario = new UsuarioDAO();		
+		List<Usuario> lista = usuario.readAll();		
+		request.setAttribute("usuario", lista);		
+		getServletContext().getRequestDispatcher("/views/usuarios.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
